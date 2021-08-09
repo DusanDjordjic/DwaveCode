c647ad1a524c15f9db688fa2043527a685028230
Dusan Djordjic <77781108+DusanDjordjic@users.noreply.github.com>
2021-08-09 15:25:37 +0200

    Landing page design 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import "../styles/globals.scss";
 import Layout from "../components/layout/Layout";
 import { AppProvider } from "../context/context";
+
 // Uvozimo "Context" i "Layout" i obavijamo ih oko nasih stranica
-// Layout nam slizu da bi u njemu napravili sidebar
+// Layout nam sluzi da bi u njemu napravili sidebar
 // Context nam sluzi da bi imali
 function MyApp({ Component, pageProps }) {
   return (
