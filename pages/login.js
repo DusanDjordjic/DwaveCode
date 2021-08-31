712a35a9d9057eb401eb82a63c3c4fcbae166b39
Dusan Djordjic <77781108+DusanDjordjic@users.noreply.github.com>
2021-08-31 17:23:31 +0200

    login form integrated

--- pages/login.js
+++ pages/login.js
@@ -5,14 +5,29 @@ import Button from "../components/layout/smallComponents/button/Button";
 // ICONS
 import { MdKeyboardArrowRight } from "react-icons/md";
 // REACT HOOKS
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 // NEXT COMPONENTS
 import Head from "next/head";
 import Link from "next/link";
+import style from "react-syntax-highlighter/dist/cjs/styles/prism/xonokai";
+
 const LoginPage = () => {
+  const initialFormState = {
+    fullName: "",
+    username: "",
+    email: "",
+    password: "",
+    name: "",
+    name: "",
+    name: "",
+  };
   // States for input fileds
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+  const [elementsX, setElementsX] = useState({ bannerX: 0, formX: 0 });
+  const [form, setForm] = useState(initialFormState);
+
+  const bannerRef = useRef();
+  const formRef = useRef();
   // Handling form submit
   const handleSubmit = (e) => {
     // Connect to db
@@ -20,10 +35,38 @@ const LoginPage = () => {
     // login user
     e.preventDefault();
   };
+  const handleChange = () => {};
+  const handleSwitch = () => {
+    const newElementsX = {
+      bannerX: bannerRef.current.clientWidth + 2,
+      formX: formRef.current.clientWidth,
+    };
+    setElementsX(newElementsX);
+    setIsLogin((prevState) => !prevState);
+  };
+  const handleResize = () => {
+    let newElementsX = {
+      bannerX: bannerRef.current.clientWidth + 2,
+      formX: formRef.current.clientWidth,
+    };
+
+    setElementsX(newElementsX);
+  };
+  useEffect(() => {
+    const newElementsX = {
+      bannerX: bannerRef.current.clientWidth + 2,
+      formX: formRef.current.clientWidth,
+    };
+    setElementsX(newElementsX);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
       <Head>
-        <title>Dwavecode | Login</title>
+        <title>Dwavecode | {isLogin ? "Login" : "Sign up"}</title>
         <meta
           name="description"
           content="Ulogujte sa na svoj nalog i pocnite sa gledanjem kurseva i pomozite nam da unapredimo sajt"
@@ -38,50 +81,70 @@ const LoginPage = () => {
               {/* Content Wrapper */}
               <section className={styles.formSection}>
                 <div className={styles.contentWrapper}>
-                  {/* Banner */}
-                  <div className={styles.banner}>
-                    <div>
-                      <h2>Dobrodošao nazad</h2>
-                      <p>Uloguj se i nastavi tamo gde si stao</p>
-                    </div>
-                    <div>
-                      <p>Nemaš nalog?</p>
-                      <Button text="Registruj se" link="/signup" />
+                  <div
+                    className={styles.bannerWrapper}
+                    ref={bannerRef}
+                    style={
+                      isLogin
+                        ? {
+                            transform: `translateX(0px)`,
+                            borderTopRightRadius: 15,
+                            borderBottomRightRadius: 15,
+                          }
+                        : {
+                            transform: `translateX(${elementsX.formX}px)`,
+                            borderTopLeftRadius: 15,
+                            borderBottomLeftRadius: 15,
+                          }
+                    }
+                  >
+                    <div className={styles.banner}>
+                      <h2>{isLogin ? "Dobrodosao nazad," : "Pozdrav,"}</h2>
+                      <p>
+                        {isLogin
+                          ? " Pariatur rem fugiat temporibus repudiandae quo, magnam velit dolores debitis perspiciatis quod!"
+                          : "Lorem ipsum dolor sit amet consectetur adipisicing elit. "}
+                      </p>
+                      <p>{isLogin ? "Nemaš nalog?" : "Već imaš nalog?"}</p>
+                      <button onClick={handleSwitch}>
+                        {isLogin ? "Registruj se" : "Uloguj se"}{" "}
+                        <MdKeyboardArrowRight />
+                      </button>
                     </div>
                   </div>
-                  {/* Form Wrapper */}
-                  <div className={styles.formWrapper}>
-                    <h1>Uloguj se</h1>
-                    <form onSubmit={(e) => handleSubmit(e)}>
-                      {/* Email */}
-                      <div className={styles.formControl}>
-                        <label htmlFor="email">Email</label>
-                        <input
-                          type="text"
-                          name="email"
-                          value={email}
-                          onChange={(e) => setEmail(e.target.value)}
-                        />
-                      </div>
-                      {/* Password */}
-                      <div className={styles.formControl}>
-                        <label htmlFor="password">Lozinka</label>
-                        <input
-                          type="password"
-                          name="password"
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                        />
-                      </div>
-                      {/* Submit Button */}
-                      <div className={styles.formControl}>
-                        <button type="submit">
-                          Uloguj se
-                          <MdKeyboardArrowRight />
-                        </button>
-                      </div>
-                      {/* To Signup */}
-                    </form>
+                  <div
+                    className={styles.formWrapper}
+                    ref={formRef}
+                    style={
+                      isLogin
+                        ? { transform: `translateX(0px)` }
+                        : {
+                            transform: `translateX(${-elementsX.bannerX}px)`,
+                          }
+                    }
+                  >
+                    <h1>{isLogin ? "Uloguj se" : "Registruj se"}</h1>
+                    <p>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Officia magni quaerat, veniam, deleniti voluptatibus modi
+                      adipisci maiores sed natus quibusdam sapiente eius laborum
+                      nam? Tenetur modi architecto, pariatur rerum impedit
+                      perspiciatis tempore hic. Quos reprehenderit enim magni
+                      eaque assumenda quam velit mollitia cupiditate molestiae
+                      vel, ab delectus recusandae et quo minima inventore ex
+                      nesciunt obcaecati, expedita magnam fugit sapiente
+                      adipisci at? Autem quia mollitia alias possimus, omnis
+                      sequi velit, temporibus recusandae molestiae, earum
+                      voluptatum similique? Veritatis, nam. Alias veniam quia
+                      ea. Reprehenderit quo molestias iusto animi eveniet vero
+                      nesciunt repellendus magnam, quisquam a distinctio,
+                      dolorum voluptatum et, nobis corporis neque? Lorem, ipsum
+                      dolor sit amet consectetur adipisicing elit. Officia magni
+                      quaerat, veniam, deleniti voluptatibus modi adipisci
+                      maiores sed natus quibusdam sapiente eius laborum nam?
+                      Tenetur modi architecto, pariatur rerum impedit
+                      perspiciatis tempore hic.
+                    </p>
                   </div>
                 </div>
               </section>
