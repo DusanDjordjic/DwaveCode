431e574d0efa8e6e6943080bc2ea742bf2199c7d
Dusan Djordjic <77781108+DusanDjordjic@users.noreply.github.com>
2021-08-31 11:41:28 +0200

    SCSS layout refactor

--- components/layout/smallComponents/button/Button.js
+++ components/layout/smallComponents/button/Button.js
@@ -1,7 +1,12 @@
 import Link from "next/link";
 import styles from "./Button.module.scss";
 import { MdKeyboardArrowRight } from "react-icons/md";
+import { useRouter } from "next/router";
 const Button = ({ text, link, fontSize = 16, isDisabled = false }) => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(link);
+  };
   if (isDisabled) {
     return (
       <button
@@ -13,12 +18,12 @@ const Button = ({ text, link, fontSize = 16, isDisabled = false }) => {
     );
   } else {
     return (
-      <button className={styles.button}>
-        <Link href={link}>
-          <a style={{ fontSize: `${fontSize}px` }}>
-            {text} <MdKeyboardArrowRight />
-          </a>
-        </Link>
+      <button
+        className={styles.button}
+        style={{ fontSize: `${fontSize}px` }}
+        onClick={handleClick}
+      >
+        {text} <MdKeyboardArrowRight />
       </button>
     );
   }
